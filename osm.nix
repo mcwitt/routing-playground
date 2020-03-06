1f0394b9f4eae074523c3675590f5c31eb78de52
Matt Wittmann <mcwitt@gmail.com>
2020-03-06 10:26:03 -0800

    Apply bounding box to osm extract

--- osm.nix
+++ osm.nix
@@ -1,22 +1,27 @@
-{ pkgs, token, extractId ? "san-francisco-bay_california" }:
+{ pkgs, token }:
 with pkgs;
 let
   osmosis = callPackage ./osmosis { };
 
-  osmPbf = let
+  extractId = "san-francisco-bay_california";
+
+  osmExtract = let
     host = "app.interline.io";
     endpoint = "osm_extracts/download_latest";
   in fetchurl {
-    name = "${extractId}.osm.pbf";
+    name = "osm-extract--${extractId}";
     url =
       "https://${host}/${endpoint}?data_format=pbf&string_id=${extractId}&api_token=${token}";
-    sha256 = "1k5fscgp1diyissw6yh9sr60ff93v34idhlgfizr2gzwf6pjs2sx";
+    sha256 = "0ra8d51dnmpxqf7b469pwyncnbjlx0174ra1l9ghvr5m05nyrv59";
   };
 
-in runCommand "osm-xml_sfbay" {
+in runCommand "osm-data-sf" {
   buildInputs = [ osmosis adoptopenjdk-jre-bin ];
 } ''
   mkdir -p $out
-  cp ${osmPbf} $out
-  osmosis --read-pbf ${osmPbf} --write-xml $out/${extractId}.osm.xml
+  cp ${osmExtract} $out
+  osmosis \
+    --read-pbf ${osmExtract} \
+    --bounding-box top=37.815 left=-122.525 bottom=37.694 right=-122.350 \
+    --write-pbf $out/sf.osm.pbf
 ''
