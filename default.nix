09848d374e1745aed7c1272d1d431e3acdaf1993
Matt Wittmann <mcwitt@gmail.com>
2020-03-31 14:16:50 -0700

    Update build

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
-{ mkDerivation, base, containers, heaps, hpack, hspec, mtl
-, postgresql-simple, stdenv
+{ mkDerivation, base, containers, heaps, hpack, hspec, monad-loops
+, mtl, postgresql-simple, stdenv
 }:
 mkDerivation {
   pname = "routing-playground";
@@ -8,14 +8,14 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base containers heaps mtl postgresql-simple
+    base containers heaps monad-loops mtl postgresql-simple
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
-    base containers heaps mtl postgresql-simple
+    base containers heaps monad-loops mtl postgresql-simple
   ];
   testHaskellDepends = [
-    base containers heaps hspec mtl postgresql-simple
+    base containers heaps hspec monad-loops mtl postgresql-simple
   ];
   prePatch = "hpack";
   homepage = "https://github.com/mcwitt/routing-playground#readme";
